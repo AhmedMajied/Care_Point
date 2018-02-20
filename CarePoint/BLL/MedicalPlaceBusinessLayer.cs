2a9f01f6e5d2a590056b12030cc3ef22591d6276
Andrew Emad <andrewen2010@yahoo.com>
2018-02-20 21:07:35 +0200

    Initial Commit

---
+++ CarePoint/BLL/MedicalPlaceBusinessLayer.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class MedicalPlaceBusinessLayer
+    {
+        private CarePointEntities DBEntities { get; set; }
+        public MedicalPlaceBusinessLayer()
+        {
+            DBEntities = new CarePointEntities();
+        }
+        public MedicalPlace GetMedicalPlace(long id)
+        {
+            return DBEntities.MedicalPlaces.SingleOrDefault(place => place.ID == id);
+        }
+    }
+}
---
+++ CarePoint/CarePoint/Controllers/MedicalPlaceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+
+namespace CarePoint.Controllers
+{
+    public class MedicalPlaceController : Controller
+    {
+        // GET: MedicalPlace
+        private MedicalPlaceBusinessLayer _medicalPlaceBusinessLayer;
+        public MedicalPlaceController()
+        {
+                
+        }
+
+        public MedicalPlaceController(MedicalPlaceBusinessLayer medicalPlaceBusinessLayer)
+        {
+            _medicalPlaceBusinessLayer = medicalPlaceBusinessLayer;
+        }
+
+        public MedicalPlaceBusinessLayer MedicalPlaceBusinessLayer
+        {
+            get
+            {
+                return _medicalPlaceBusinessLayer ?? new MedicalPlaceBusinessLayer();
+            }
+            private set
+            {
+                _medicalPlaceBusinessLayer = value;
+            }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

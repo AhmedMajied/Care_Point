b8932da188720b34351a7e6b146967ea6a48b78d
Ahmed-Hussein-Karam <ahkcsit@gmail.com>
2018-02-21 08:45:03 +0200

    updateICUCount, prescriptionTab and newWorkplace are added

--- CarePoint/CarePoint/Scripts/sos.js
+++ CarePoint/CarePoint/Scripts/sos.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     popoverContent = `<div id='idiv-sos-pop'>
                         <form>
                             <textarea class='form-control input-lg' placeholder="What's wrong ?!" rows=10 cols=25 style='resize: none;'></textarea>
-                            <input type='submit' value='Send' class='btn btn-danger'>
+                            <input type='submit' value='Send' class='btn btn-danger' style='width: 100%; margin-top: 1em;'>
                         </form>
                     </div>`;
     $('#ibtn-sos-pop').popover({

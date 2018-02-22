a897d6eb26ac338e42c3e7345dbe7449f311bd6e
Andrew Emad <andrewen2010@yahoo.com>
2018-02-22 21:34:51 +0200

    MedicalPlaceViewModel

--- CarePoint/CarePoint/Controllers/MedicalPlaceController.cs
+++ CarePoint/CarePoint/Controllers/MedicalPlaceController.cs
@@ -40,7 +40,60 @@ namespace CarePoint.Controllers
 
         public ActionResult ProfilePage(long id)
         {
-            MedicalPlaceProfileViewModel model = new MedicalPlaceProfileViewModel();
+            var medicalPlace = MedicalPlaceBusinessLayer.GetMedicalPlace(id);
+            MedicalPlaceProfileViewModel model = new MedicalPlaceProfileViewModel()
+            {
+                Services = new List<ServiceViewModel>()
+            };
+            
+            foreach (var service in medicalPlace.Services)
+            {
+                var groupedSlots = service.WorkSlots.GroupBy(slot => new { slot.StartTime, slot.EndTime });
+                ServiceViewModel smodel = new ServiceViewModel()
+                {
+                    ID=service.ID,
+                    Name=service.Name,
+                    Cost=service.Cost??0,
+                    Description=service.Description,
+                    CategoryID=service.CategoryID,
+                    WorkSlots = new List<WorkSlotViewModel>()
+                };
+                foreach (var element in groupedSlots)
+                {
+                    WorkSlotViewModel wmodel = new WorkSlotViewModel();
+                    wmodel.StartTime = element.Key.StartTime ?? TimeSpan.Zero;
+                    wmodel.EndTime = element.Key.EndTime ?? TimeSpan.Zero;
+                    foreach(var day in element)
+                    {
+                        switch (day.DayName)
+                        {
+                            case "Saturday":
+                                wmodel.IsSaturday = true;
+                                break;
+                            case "Sunday":
+                                wmodel.IsSunday = true;
+                                break;
+                            case "Monday":
+                                wmodel.IsMonday = true;
+                                break;
+                            case "Tuesday":
+                                wmodel.IsTuesday = true;
+                                break;
+                            case "Wednesday":
+                                wmodel.IsWednesday = true;
+                                break;
+                            case "Thursday":
+                                wmodel.IsThursday = true;
+                                break;
+                            case "Friday":
+                                wmodel.IsFriday = true;
+                                break;
+                        }
+                        smodel.WorkSlots.Add(wmodel);
+                    }
+                    model.Services.Add(smodel);
+                }
+            }
             return View(model);
         }
     }
--- CarePoint/CarePoint/Models/MedicalPlaceViewModels.cs
+++ CarePoint/CarePoint/Models/MedicalPlaceViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,51 @@ namespace CarePoint.Models
 {
     public class MedicalPlaceProfileViewModel
     {
+        public ICollection<ServiceViewModel> Services { get; set; }
+
         public Service NewService { get; set; }
+    }
+
+    public class ServiceViewModel
+    {
+        public long ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public long CategoryID { get; set; }
+
+        [Required]
+        public decimal Cost { get; set; }
+
+        public ICollection<WorkSlotViewModel> WorkSlots { get; set; }
+
+        public WorkSlot NewWorkSlot { get; set; }
+    }
+
+    public class WorkSlotViewModel
+    {
+        public bool IsSaturday { get; set; }
+
+        public bool IsSunday { get; set; }
+
+        public bool IsMonday { get; set; }
+
+        public bool IsTuesday { get; set; }
+
+        public bool IsWednesday { get; set; }
+
+        public bool IsThursday { get; set; }
+
+        public bool IsFriday { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
     }
 }

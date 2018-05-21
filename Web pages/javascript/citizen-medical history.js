e5c6d64066348df84920728179843bd4729cd868
Andrew Emad <andrewen2010@yahoo.com>
2018-05-21 19:08:43 +0200

    prepare for merge

--- Web pages/javascript/citizen-medical history.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#itbl-medical-history').DataTable({
-        "order": [[0, "desc"]],
-        "iDisplayLength": 10,
-        "bPaginate": true,
-        "sPaginationType": "full_numbers"
-    });
-});
--- Web pages/javascript/modal.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.modal').attr('data-keyboard','false');
-    $('.modal').attr('data-backdrop','static');
-    $(".cbtn-modal").click(function(){
-        event.preventDefault();
-    });
-});

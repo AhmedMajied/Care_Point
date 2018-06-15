c1067072d59bde1e07c15d546b547905c4222de2
mariam_ashraf_fekry <mariamashraf096@gmail.com>
2018-06-15 23:06:28 +0200

    addPharmacy as MedicalPlace & SearchMedicine

---
+++ CarePoint/BLL/PharmacyBusinessLayer.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PharmacyBusinessLayer
+    {
+        private CarePointEntities DBEntities;
+
+        public PharmacyBusinessLayer()
+        {
+            DBEntities = new CarePointEntities();
+        }
+        public void AddPharmacy(Pharmacy pharmacy)
+        {
+            DBEntities.Pharmacies.Add(pharmacy);
+            DBEntities.SaveChanges();
+        }
+        public Pharmacy GetPharmacy(long id)
+        {
+            return DBEntities.Pharmacies.SingleOrDefault(pharmacy => pharmacy.ID == id);
+        }
+        public ICollection<Pharmacy> SearchMedicinePharmacies(string drugName, string location)
+        {
+            List<Pharmacy> pharmacies = new List<Pharmacy>();
+            List<Pharmacy> sortedPhrmacies = new List<Pharmacy>();
+            pharmacies = DBEntities.PharmacyMedicines.Where(m => m.Medicine.Name.Contains(drugName) && m.Quantity>0).Select(p => p.Pharmacy).ToList();
+            pharmacies = pharmacies.GroupBy(pharmacy => pharmacy.ID).Select(pharmacy=>pharmacy.First()).ToList();
+            sortedPhrmacies = pharmacies.OrderByDescending(m => m.Location.Distance(DbGeography.FromText(location, 4326))).ToList();
+            return sortedPhrmacies;
+        }
+    }
+}
